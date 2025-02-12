b91a3823c947097521ff319b9c06a509d9ae419f
Igor Micadei <i.micadei@gmail.com>
2025-02-12 15:54:16 -0300

    updated project name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="StreamlitSessionState",
+    name="streamlit-meta-state",
     version="0.1.0",
     packages=find_packages(),
     install_requires=[
